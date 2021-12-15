7f43bda5755cb569122a03718d10c52400e7d1a7
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-12-14 22:12:08 -0500

    Proyecto Journal App | Estilos <JournalEntries/>

---
+++ src/journal/JournalEntry.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+            <div
+                className="jouranl__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://th.bing.com/th/id/R.99cb7a88cc3e20e78e18ba7aa6c6ad7b?rik=SZQbCF3eH9aLPA&pid=ImgRaw&r=0)'
+                }}
+            >
+            </div>
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo dia
+                </p>
+                <p className="journal__entry-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
