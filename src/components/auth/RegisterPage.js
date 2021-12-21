1cdddf31f548b113b9d88e0ef04a223adc1fa341
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-12-21 17:49:52 -0500

    Proyecto Journal App | Formulario de registro de usuarios.

--- src/components/auth/RegisterPage.js
+++ src/components/auth/RegisterPage.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterPage = () => {
+    const [formValues, handleInputChange] = useForm({
+        name: 'Evelin',
+        email: 'evelinfranco7@gmail.com',
+        password: 123456,
+        password2: 123456
+    });
+
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(name, email, password, password2);
+    }
+
     return (
 
         <>
             <h1 className="auth__title">Register Page</h1>
-            <form>
+            <form onSubmit={handleRegister}>
                 <input
                     type="text"
                     placeholder="Name"
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
                 />
                 <input
                     type="text"
@@ -20,18 +37,24 @@ export const RegisterPage = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input
                     type="password"
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value={password}
+                    onChange={handleInputChange}
                 />
                 <input
                     type="password"
                     placeholder="Confirm password"
                     name="password2"
                     className="auth__input"
+                    value={password2}
+                    onChange={handleInputChange}
                 />
                 <button
                     type="submit"
